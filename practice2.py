26297812efc162eee53397d694158df07a4680c9
Hamza Amjad <amjadamjad525@gmail.com>
2021-08-26 21:49:06 +0500

    Date of Birth after signup

--- practice2.py
+++ practice2.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.proxy import *
 import json
+import time
 
 #browser open
 
@@ -14,7 +15,7 @@ options.add_argument("start-maximized")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 driver = webdriver.Chrome(options=options, executable_path=r"C:\repos\practice\My-Practice\chromedriver.exe")
-driver.get('https://www.instagram.com/accounts/emailsignup/?hl=en')
+driver.get('https://www.instagram.com/accounts/emailsignup/?hl=en') 
 
 with open('C:/repos/practice/My-Practice/data.json') as f:
   data = json.load(f)
@@ -22,11 +23,23 @@ for d in data:
    WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='emailOrPhone']"))).send_keys(d['emailOrPhone'])
    driver.find_element_by_xpath("//input[@name='fullName']").send_keys(d['fullName'])
    driver.find_element_by_xpath("//input[@name='username']").send_keys(d['username'])
-   explore=driver.find_element_by_xpath("//button[@class='sqdOP yWX7d    y3zKF    ']")
-   explore.click()
+  #  driver.find_element_by_xpath(
+  #           "//span[contains(concat(' ', @class, ' '), ' coreSpriteInputRefresh ')]/parent::button")
+   
    driver.find_element_by_xpath("//input[@name='password']").send_keys(d['password'])
+   driver.find_element_by_xpath("//button[text()='Sign up']").click()
+time.sleep(8)
+driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select").click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select/option[4]"))).click()
 
-# WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Sign up']")))
+driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select").click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select/option[10]"))).click()
+
+driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select").click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select/option[27]"))).click()
+
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[6]/button"))).click()
+time.sleep(3)
 
 
 
